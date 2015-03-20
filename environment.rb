d5df624510c6d76613d68b46a0edc2e0776bad23
suprfrye <tara.c.frye@gmail.com>
2015-03-19 20:19:45 -0400

    Set view paths in environment.rb file

--- environment.rb
+++ environment.rb
@@ -9,6 +9,13 @@ require 'rubygems'
 require 'uri'
 require 'pathname'
 
+require 'erb'
+
+configure do
+  set :root. APP_ROOT.to_path
+  set :views, File.join(APP_ROOT, "app", "views")
+end
+
 
 APP_ROOT = Pathname.new(File.expand_path('../', __FILE__))
 APP_NAME = APP_ROOT.basename.to_s
