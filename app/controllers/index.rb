b9423800a862f93ddcd29409c51368193fddde0e
suprfrye <tara.c.frye@gmail.com>
2015-03-19 21:40:22 -0400

    Update show view and add put view

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -18,12 +18,23 @@ get '/sealions/:id' do
   erb :show
 end
 
-put '/sealions/:id' do
-  redirect '/sealions'
-end
-
 delete '/sealions/:id' do
   lion = SeaLion.find_by(id: params[:id])
   lion.destroy
   redirect '/sealions'
 end
+
+get '/sealions/:id/edit' do
+  @lion = SeaLion.find(params[:id])
+  erb :update
+end
+
+put '/sealions/:id/edit' do
+  @lion = SeaLion.find(params[:id])
+  @lion.update_attributes(name: params[:name],
+                         location: params[:location],
+                         favorite_food: params[:favorite_food])
+  redirect "/sealions/#{@lion.id}"
+end
+
+
